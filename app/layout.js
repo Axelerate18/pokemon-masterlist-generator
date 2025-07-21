453ccc17e89e3b2c4ac46ca5b8ff72781c91a6cb
Axel Verbeke <axelverbeke18@gmail.com>
2025-07-21 22:44:00 +0200

    Add keyboard navigation to search suggestions

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,12 @@ export default function RootLayout({ children }) {
   return (
         <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Pokémon TCG Masterlist Generator</title>
-      </head>
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes"
+  />
+  <title>Pokémon TCG Masterlist Generator</title>
+</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
